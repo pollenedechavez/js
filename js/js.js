c34871ce9ed4361ec90dcd0567412ac8e6d41e34
pollenedechavez <pollenejoydechavez681@gmail.com>
2024-05-21 17:09:51 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,13 @@
+function changecolor(){
+    document.getElementById('JavaScript').style.color="blue";
+}
+function changecolor2(){
+    document.getElementById('JavaScript').style.color="red";
+}
+function changeimage(){
+    document.getElementById('Smile').src="images/smile.jpg";
+}
+function changetext(){
+     txt=document.getElementById("input1").value;
+    document.getElementById('JavaScript').innerHTML=txt;
+}
